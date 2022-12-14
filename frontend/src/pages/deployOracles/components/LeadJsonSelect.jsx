f439beecef4ca681109a1fce5e2ff25cb49e9a06
Jupiteriana <xiong0201y@163.com>
2022-12-14 21:30:31 +0800

    add json animation

--- frontend/src/pages/deployOracles/components/LeadJsonSelect.jsx
+++ frontend/src/pages/deployOracles/components/LeadJsonSelect.jsx
@@ -2,7 +2,7 @@ import styled, { keyframes } from 'styled-components';
 import { useState } from 'react';
 import classNames from 'classnames';
 import JsonTree from '@/components/comm/JsonTree';
-import { ReactComponent as Vector } from '@/assets/imgs/svg/vector.svg';
+import { ReactComponent as MouseIcon } from '@/assets/imgs/svg/vector.svg';
 
 const Wrap = styled.div`
   margin-top: 12px;
@@ -10,15 +10,15 @@ const Wrap = styled.div`
   border-radius: 20px;
   overflow: hidden;
   padding: 20px;
-  /* position: absolute; */
   >.mask-wrap{
+    pointer-events: none;
     position: absolute;
     left: 0;
     right: 0;
     top: 0;
     bottom: 0;
-    background-color: rgba(0, 0, 0, 0.5);
     padding: 12px;
+
     >.first-text{
       position: absolute;
       transition: all .3s;
@@ -27,6 +27,93 @@ const Wrap = styled.div`
         /* transform: matrix(); */
         transform: translate3d(100px,100px, 0);
       }
+    }
+  }
+`;
+
+const mouseWave = keyframes`
+  from {
+    transform: scale(1);
+    opacity: 1;
+    border: 2px solid #1781ef;
+    box-shadow:  0 0 20px #1781ef;
+  }
+
+  to {
+    transform: scale(3);
+    opacity: 0;
+  }
+`;
+
+const MouseWrap = styled.div`
+  position: relative;
+  animation: moveX1 0.5s forwards, moveX2 0.5s forwards 1s;
+  animation-timing-function: linear;
+  >div{
+    animation: moveY1 0.5s forwards, moveY2 0.5s forwards 1s;
+    animation-timing-function: ease-in;
+    >svg{
+      animation: clickScale 0.2s 0.6s,  clickScale 0.2s 1.6s;
+      animation-timing-function: ease-out ;
+      animation-direction: alternate-reverse;
+    }
+    &::after{
+      display: inline-block;
+      content: '';
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      z-index: -1;
+      position: absolute;
+      top: -12px;
+      left: -12px;
+      animation: ${mouseWave} 0.5s forwards linear 0.7s, ${mouseWave} 0.5s forwards linear 1.7s;
+    }
+  }
+
+  @keyframes clickScale {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(0.6);
+    }
+  }
+
+
+  @keyframes moveX1 {
+    from {
+      transform: translateX(0);
+    }
+    to {
+      transform: translateX(156px);
+    }
+  }
+
+  @keyframes moveX2 {
+    from {
+      transform: translateX(156px);
+    }
+    to {
+      transform: translateX(130px);
+    }
+  }
+
+  @keyframes moveY1 {
+    from {
+      transform: translateY(0);
+    }
+    to {
+      transform: translateY(90px);
+    }
+  }
+
+  @keyframes moveY2 {
+    from {
+      transform: translateY(90px);
+    }
+    to {
+      transform: translateY(230px);
     }
   }
 `;
@@ -82,9 +169,11 @@ function LeadJsonSelect() {
           first
         </p> */}
         {/* <VectorIcon className="absolute w-4 h-4" /> */}
-        {/* <div>
-          <Vector className="absolute top-10 w-4 h-4" />
-        </div> */}
+        <MouseWrap>
+          <div>
+            <MouseIcon fill="white" className=" w-4 h-4" />
+          </div>
+        </MouseWrap>
       </div>
     </Wrap>
   );
